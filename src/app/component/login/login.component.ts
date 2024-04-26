90cb16b7e57ca39644134b8aa85cc5e331de2773
ramalingam-chitra <chitraramalingam36@gmail.com>
2024-04-26 20:36:19 +0200

    feat: basic login validation added

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms'
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,15 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required])
+  });
+  showError: boolean = false;
+  showPwdError: boolean = false;
 
   constructor(private router: Router) { }
-
   ngOnInit() {}
 
    /* Function: redirectToOrders
@@ -18,6 +26,15 @@ export class LoginComponent implements OnInit {
    * Return: none
    */
    redirectToOrders() {
-    this.router.navigate(["orders"]);
+     if(this.loginForm.controls.email.value == ''){
+      this.showError = true;
+     }
+     if(this.loginForm.controls.password.value == '') {
+      this.showPwdError = true;
+     }
+     if(this.loginForm.valid) {
+      this.router.navigate(["orders"]);
+     } 
+
   }
 }
